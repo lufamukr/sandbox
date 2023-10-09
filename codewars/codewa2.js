ce05d3c7e67be1a906bb64839a31959c9c87ea90
lufamukr <lufamukr@gmail.com>
2023-10-09 12:46:57 +0200

    file escape sequences

--- codewars/codewa2.js
+++ codewars/codewa2.js
@@ -1,10 +1,7 @@
 'use strict';
 
-function numWilson(num) {
-    for(let i = 0; i < 5; i++) {
-        i = ((i * (i-1)) + 1) / (i * i);
-        return i;
-    }
+function faktorial() {
+
 }
 
-numWilson();
+faktorial();
---
+++ freecodecamp/freeccamp24.js
@@ -0,0 +1,5 @@
+'use strict';
+// Escape sequences
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr);
