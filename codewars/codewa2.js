39ce6c38930c1e5ca2c9ccb711c15d47c8c228a9
lufamukr <lufamukr@gmail.com>
2023-10-09 16:03:48 +0200

    09.10

--- codewars/codewa2.js
+++ codewars/codewa2.js
@@ -1,7 +1,13 @@
 'use strict';
+// Calculate Price Excluding VAT
 
-function faktorial() {
-
+function excludingVatPrice(price){
+    // your code
+    let res = price - (price * 15 / 115);
+    if(price === undefined || price === null) {
+        res = -1;
+    } else {
+        res = Math.round(res * 100) / 100;
+    }
+    return res;
 }
-
-faktorial();
