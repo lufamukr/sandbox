7bd347a998c24f4e21d941f4f3543f9d7ccc3fb9
lufamukr <lufamukr@gmail.com>
2023-10-12 16:01:27 +0200

    codewa5 add

--- codewars/codewa5.js
+++ codewars/codewa5.js
@@ -1,12 +1,22 @@
 'use strict';
 
 function solution(str, ending){
-    // TODO: complete
-    let leng = ending.length;
-    let le2ng = str.length - ending.length;
-    console.log(leng);
-    for(let i = 0; i <= leng; i++) {
-      console.log(ending[i] == str[le2ng+i]);
+  // TODO: complete
+  let leng = ending.length;
+  let le2ng = str.length - ending.length;
+  let curEnd = '';
+ 
+  for(let i = 0; i < leng; i++) {
+    if((ending[i] === str[le2ng+i])) {
+      curEnd += str[le2ng+i];
+    } 
   }
+    if(curEnd === ending) {
+      console.log(true);
+      return true;
+    } else {
+      return false;
+    }
 }
-solution('ask', 'sk');
+
+solution('end', 'nd');
