3bd86a1ce4a29ca8d1c9d309777f6dc88b041a93
lufamukr <lufamukr@gmail.com>
2023-10-15 10:17:35 +0200

    ? cw7

---
+++ codewars/codewa7.js
@@ -0,0 +1,12 @@
+'use strict';
+// Your task is to make a function hotpo that takes a positive n as input and returns the number of times you need to perform this algorithm to get n = 1
+
+
+let hotpo = function(n){
+    if(n == 0) return 0; //Optional Handler to n = 0
+    
+}
+
+hotpo(4);
+
+
