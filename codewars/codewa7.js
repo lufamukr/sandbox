722742fb5c4dec4646c0bb93b40c50fd3a53ad97
lufamukr <lufamukr@gmail.com>
2023-10-15 19:07:14 +0200

    unfinish cw7 !!!

--- codewars/codewa7.js
+++ codewars/codewa7.js
@@ -2,11 +2,21 @@
 // Your task is to make a function hotpo that takes a positive n as input and returns the number of times you need to perform this algorithm to get n = 1
 
 
-let hotpo = function(n){
-    if(n == 0) return 0; //Optional Handler to n = 0
-    
+let hotpo = function(n) {
+    let res = '';
+    if(n == 0) return 0; 
+    for(let i = 0; i <= 5; i++) {
+        if(n % 2 === 1) {
+            n = (n * 3) - 1;
+        }   else {
+            n = n / 2;
+        }
+        res = res + i;
+    }
+    console.log(res.length);
+    return res.length;
 }
+   
 
-hotpo(4);
-
+hotpo(5);
 
